fbdab460cdf8683dd59d54d7f85d6058d0965abf
Aashiqui <ashikmail2747@gmail.com>
2025-05-28 14:25:44 +0530

    added g pattern

---
+++ Day 2/Alphabet Pattern/DemoG.java
@@ -0,0 +1,14 @@
+public class DemoG {
+    public static void main(String[] args) {
+        for(int row=0;row<7;row++){
+            for(int col=0;col <5;col++){
+                if(col==0||row==0||row==6||col==4&&row>2||row==3&&col==3){
+                    System.out.print("*");
+                }else{
+                    System.out.print( " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
