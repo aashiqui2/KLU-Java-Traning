5d97d37b2263a7828bddfcc307e23c4421cfb6ba
Aashiqui <ashikmail2747@gmail.com>
2025-05-28 15:20:02 +0530

    added V pattern

---
+++ Day 2/Alphabet Pattern/DemoV.java
@@ -0,0 +1,14 @@
+public class DemoV {
+    public static void main(String[] args) {
+        for(int row=0;row<6;row++){
+            for(int col=0;col<11;col++){
+                if(row==col||row+col==10){
+                    System.out.print("* ");
+                }else{
+                    System.out.print( "  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
