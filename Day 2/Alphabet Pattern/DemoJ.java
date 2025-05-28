e3564d7c11c9a208e406077bdb0cbb1b0a4ceaed
Aashiqui <ashikmail2747@gmail.com>
2025-05-28 14:37:14 +0530

    added J pattern

---
+++ Day 2/Alphabet Pattern/DemoJ.java
@@ -0,0 +1,14 @@
+public class DemoJ {
+    public static void main(String[] args) {
+        for (int row = 0; row < 7; row++) {
+            for (int col = 0; col < 5; col++) {
+                if (row == 0 || col == 2 || row == 6 && col < 2 || col == 0 && row == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
