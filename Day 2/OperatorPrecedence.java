ad27b5898d05688d0ae2a130f30558ed0b55c004
Aashiqui <ashikmail2747@gmail.com>
2025-05-29 09:02:28 +0530

    added operator precedence

---
+++ Day 2/OperatorPrecedence.java
@@ -0,0 +1,19 @@
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+       /*  boolean x = false, y = true, z = false;
+
+        boolean ans = x == y == z;
+
+        System.out.println(ans); */
+
+        /* int x = 5;
+
+		int ans = ++++x;//error
+
+		System.out.println(ans);  */
+
+        int e = 20 ^ ((4 * 5) / 3) - 20 + 1;
+        System.out.printf("%d",e);
+
+    }
+}
