0fee07e084a5f93c788bf5f7f0625270cf67e83a
Aashiqui <ashikmail2747@gmail.com>
2025-05-30 14:38:24 +0530

    addd new problem

---
+++ Day 4/Number Crunching/Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int n = 12345;
+        int sum = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+        System.out.println(sum);
+    }
+}
