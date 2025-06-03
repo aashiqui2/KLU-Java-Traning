3916b185e2df4f83674331b7291b75455a046d9d
Aashiqui <ashikmail2747@gmail.com>
2025-06-03 14:27:36 +0530

    added new code

--- Day 7/Demo5.java
+++ Day 7/Demo5.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class Demo5 {
     public static void main(String[] args) {
         int a[] = { 1, 3, 4, 2, 3, 2, 2, 4, 4 };
         int n = a.length;
-        boolean b[] = new boolean[n];
-        for (int i = 0; i < n; i++) {
+        //! brute force approach
+       /* boolean b[] = new boolean[n];
+          for (int i = 0; i < n; i++) {
             if (!b[i]) {
                 int count = 0;
                 for (int j = 0; j < n; j++) {
@@ -16,6 +19,30 @@ public class Demo5 {
                     System.out.println(a[i] + " comes " + count + " times");
                 }
             }
+        } */
+    //! Time complexity:O(n^2)
+
+    //! Optimal approach
+      Arrays.sort(a);
+      int max=0;
+      for(int val:a)
+      {
+        if(val>max)
+        {
+            max=val;
         }
+      }
+      int dp[]=new int[max+1];
+      for(int i=0;i<n;i++)
+      {
+        dp[a[i]]++;
+      }
+      for(int i=0;i<max+1;i++)
+      {
+        if(dp[i]>0)
+        {
+            System.out.println(i+" comes "+dp[i]+" times");
+        }
+      }
     }
 }
