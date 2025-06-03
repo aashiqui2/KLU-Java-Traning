64b477ded1c0fcddbbad044998b34ffe3dfaa9cd
Aashiqui <ashikmail2747@gmail.com>
2025-06-03 12:08:26 +0530

    added new code

---
+++ Day 7/Demo4.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        int a[]={0,1,0,3,12};
+        //! brute force approach
+        int cntZero=0;
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            {
+                cntZero++;
+            }
+        }
+        int idx=0;
+        for(int i=0;i< n;i++)
+        {
+            if(a[i]!=0)
+            {
+                a[idx]=a[i];
+                idx++;
+            }
+        }
+        for(int i=0;i<cntZero;i++)
+        {
+            a[idx]=0;
+            idx++;
+        }
+        System.out.println(Arrays.toString(a));
+        //Time complexity:O(n)+O(n)+O(n)=O(3*n)
+    }
+}
