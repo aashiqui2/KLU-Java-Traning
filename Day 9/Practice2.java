65e3d9fd15af72ed0a476bc86ed59926dcd772ac
Aashiqui <ashikmail2747@gmail.com>
2025-06-05 12:32:52 +0530

    modified string palindrome

--- Day 9/Practice2.java
+++ Day 9/Practice2.java
@@ -1,36 +1,34 @@
 public class Practice2 {
     public static void main(String[] args) {
-        // String s="A man, a plan, a canal: Panama";
+        String s = "A man, a plan, a canal: Panama";
         // String s="race a car";
 
         // amanaplanacanalpanama
         System.out.println(isPalindrome(s));
     }
-     public static  boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        if(s.isEmpty())
-        {
-            return false;
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        if (s.isEmpty()) {
+            return true;
         }
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j)
-        {
-           
-            while(Character.isLetterOrDigit(i))
-            {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            char start=s.charAt(i);
+            char end=s.charAt(j);
+
+            while (!Character.isLetterOrDigit(start)) {
                 i++;
             }
-            while(Character.isLetterOrDigit(j))
-            {
+            while (!Character.isLetterOrDigit(end)) {
                 j--;
             }
-            if(s.charAt(i)!=s.charAt(j))
-            {
+            if (start !=end) {
                 return false;
             }
-                i++;
-                j--;
+            i++;
+            j--;
         }
         return true;
     }
